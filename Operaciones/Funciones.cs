dd1d7f9ea405f06bfc54a0c57732882c6730a321
Maty2211 <matiasycortes@gmail.com>
2024-11-14 09:46:57 -0300

    Add files via upload

---
+++ Operaciones/Funciones.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones
+{
+    public class Funciones
+    {
+        public int Potencia(int numA, int numB)
+        {
+            int potencia = numA;
+            for (int i = 0; i < numB; i++)
+            {
+                potencia = potencia * numA;
+            }
+            return potencia;
+        }
+
+        public float Dividir(int numA,int numB)
+        {
+            if (numB != 0)
+            {
+                return numA / numB;
+            }
+            else{
+                Console.WriteLine("No se puede dividir entre 0");
+            }
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("Ingrese un número");
+            int numA = Console.ReadLine();
+            Console.WriteLine("Ingrese otro número");
+            int numB = Console.ReadLine();
+
+            Console.WriteLine("Eija una opcion:");
+            Console.WriteLine("\n   (1) Potencia \n   (2) Dividir");
+            int menu = Console.ReadLine();
+            switch (menu)
+            {
+                case 1:
+                    Potencia(numA, numB);
+                case 2:
+                    Dividir(numA, numB);
+            }
+        }
+    }
+}
---
+++ TrabajoPractico1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoPractico1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numeroA = Convert.ToInt32(textBox1.Text);
+            int numeroB = Convert.ToInt32(textBox2.Text);
+
+            textBox3.Text= (numeroA + numeroB).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int numeroA = Convert.ToInt32(textBox1.Text);
+            int numeroB = Convert.ToInt32(textBox2.Text);
+
+            textBox4.Text = (numeroA+""+numeroB).ToString();
+        }
+    }
+}
